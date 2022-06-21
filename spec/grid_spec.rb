1d55b985f19ef5352363235fc332d6424fdb62cd
matristle <97355883+matristle@users.noreply.github.com>
2022-06-22 02:06:04 +0300

    Add test for checking first row X win

---
+++ spec/grid_spec.rb
@@ -0,0 +1,29 @@
+require './lib/grid'
+require './lib/player'
+
+describe Grid do
+  
+  describe '#check_for_win' do
+  let(:player) { Player.new }
+  
+  context 'when XXX on first row' do
+    let(:grid_array) do 
+      [ 
+        [' x ', ' x ', ' x '],
+        [' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . '] 
+      ]
+    end
+    subject(:grid) { Grid.new(grid_array) }
+
+      it 'returns a @won of true' do
+        grid.check_for_win(player)
+        expect(player.won).to eq(true)
+      end
+    end
+  end
+    
+end
+
+
+
