4f50f824e74a066a55088d6a4d1e9163081b7eb2
matristle <97355883+matristle@users.noreply.github.com>
2022-06-22 23:34:36 +0300

    Write basic tests for last 2 rows

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -4,17 +4,49 @@ require './lib/player'
 describe Grid do
   
   describe '#check_for_win' do
-  let(:player) { Player.new }
+    let(:player) { Player.new }
   
-  context 'when XXX on first row' do
-    let(:grid_array) do 
-      [ 
-        [' x ', ' x ', ' x '],
-        [' . ', ' . ', ' . '],
-        [' . ', ' . ', ' . '] 
-      ]
+    context 'when XXX on first row' do
+      let(:grid_array) do 
+        [ 
+          [' x ', ' x ', ' x '],
+          [' . ', ' . ', ' . '],
+          [' . ', ' . ', ' . '] 
+        ]
+      end
+      subject(:grid) { Grid.new(grid_array) }
+
+      it 'returns a @won of true' do
+        grid.check_for_win(player)
+        expect(player.won).to eq(true)
+      end
     end
-    subject(:grid) { Grid.new(grid_array) }
+
+    context 'when XXX on second row' do
+      let(:grid_array) do 
+        [ 
+          [' . ', ' . ', ' . '],
+          [' x ', ' x ', ' x '],
+          [' . ', ' . ', ' . '] 
+        ]
+      end
+      subject(:grid) { Grid.new(grid_array) }
+
+      it 'returns a @won of true' do
+        grid.check_for_win(player)
+        expect(player.won).to eq(true)
+      end
+    end
+
+    context 'when XXX on third row' do
+      let(:grid_array) do 
+        [ 
+          [' . ', ' . ', ' . '],
+          [' . ', ' . ', ' . '], 
+          [' x ', ' x ', ' x ']
+        ]
+      end
+      subject(:grid) { Grid.new(grid_array) }
 
       it 'returns a @won of true' do
         grid.check_for_win(player)
@@ -22,7 +54,6 @@ describe Grid do
       end
     end
   end
-    
 end
 
 
